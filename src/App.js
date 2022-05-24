a4b7d59daef8c603be571f1eabf212d529bbc790
singhankita29 <100141535+singhankita29@users.noreply.github.com>
2022-05-25 02:11:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Todo from './components/Todo';
+// function App() {
+//   return (
+//     <div className="App">
+//       <Todo></Todo>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Todoapp from './components/todoapp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Todoapp/>
+    </div>
+  );
+}
+
+export default App;
